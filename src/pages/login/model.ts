28d3b4c8c24c1205b5ab3d4b9784a7fd762274ac
Thái Tấn Lợi <thaitanloi365@gmail.com>
2020-08-17 01:19:03 +0700

    fix log

--- src/pages/login/model.ts
+++ src/pages/login/model.ts
@@ -1,14 +1,12 @@
+import {login} from 'services/api/auth';
 import store from 'store';
+import {IEffect, IModel} from 'types';
 import router from 'umi/router';
-import { IEffect, IModel } from 'types';
-import { parseFromUrl } from 'utils';
-import { login } from 'services/api/auth';
+import {parseFromUrl} from 'utils';
 
 export interface ILoginModelState {}
 export interface ILoginModelType extends IModel<ILoginModelState> {
-  effects: {
-    login: IEffect;
-  };
+  effects: {login: IEffect;};
 }
 
 const Model: ILoginModelType = {
@@ -17,22 +15,23 @@ const Model: ILoginModelType = {
   state: {},
 
   effects: {
-    *login({ payload }, { put, call, select }) {
-      const { success, data } = yield call(login, payload);
-      if (success && data) {
-        store.set('token', data.token);
-        store.set('user', data.user);
-        var value = parseFromUrl(location?.search);
-        if (value?.from) {
-          router.push(value?.from);
-          return;
-        }
+    *
+        login({payload}, {put, call, select}) {
+          const {success, data} = yield call(login, payload);
+          if (success && data) {
+            store.set('token', data.token);
+            store.set('user', data.user);
+            var value = parseFromUrl(location?.search);
+            if (value?.from && value?.from !== '/login') {
+              router.push(value?.from);
+              return;
+            }
 
-        router.push('/dashboard');
-      } else {
-        throw data;
-      }
-    },
+            router.push('/dashboard');
+          } else {
+            throw data;
+          }
+        },
   },
 };
 
